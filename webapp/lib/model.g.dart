466f0606f1e4f6c3830bed7ced12e6ac70531329
danrubel <dan.rubel.usa@gmail.com>
2019-10-16 14:51:06 -0400

    refactor enum to/from string

--- webapp/lib/model.g.dart
+++ webapp/lib/model.g.dart
@@ -4,8 +4,28 @@ enum MessageDirection {
   In,
   Out,
 }
+MessageDirection MessageDirection_fromString(String text, [MessageDirection defaultValue = MessageDirection.Out]) {
+  if (text == 'in') return MessageDirection.In;
+  if (text == 'out') return MessageDirection.Out;
+  return defaultValue;
+}
+String MessageDirection_toString(MessageDirection value, [String defaultText = 'out']) {
+  if (value == MessageDirection.In) return 'in';
+  if (value == MessageDirection.Out) return 'out';
+  return defaultText;
+}
 
 enum TagType {
   Normal,
   Important,
+}
+TagType TagType_fromString(String text, [TagType defaultValue = TagType.Normal]) {
+  if (text == 'normal') return TagType.Normal;
+  if (text == 'important') return TagType.Important;
+  return defaultValue;
+}
+String TagType_toString(TagType value, [String defaultText = 'normal']) {
+  if (value == TagType.Normal) return 'normal';
+  if (value == TagType.Important) return 'important';
+  return defaultText;
 }
