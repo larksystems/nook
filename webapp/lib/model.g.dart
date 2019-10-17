10f178419dbc5a88204fa45935ee0466a2cbfaa2
Dan Rubel <dan@lark.systems>
2019-10-17 12:25:33 -0400

    extract enum to/from string (#134)

--- webapp/lib/model.g.dart
+++ webapp/lib/model.g.dart
@@ -1,11 +1,32 @@
-// Generated model
+// This generated file is used by `model.dart`
+// and should not be imported or exported by any other file.
 
 enum MessageDirection {
   In,
   Out,
 }
+MessageDirection MessageDirection_fromString(String text, [MessageDirection defaultValue = MessageDirection.Out]) {
+  if (text == 'in') return MessageDirection.In;
+  if (text == 'out') return MessageDirection.Out;
+  return defaultValue;
+}
+String MessageDirection_toString(MessageDirection value, [String defaultText = 'out']) {
+  if (value == MessageDirection.In) return 'in';
+  if (value == MessageDirection.Out) return 'out';
+  return defaultText;
+}
 
 enum TagType {
   Normal,
   Important,
+}
+TagType TagType_fromString(String text, [TagType defaultValue = TagType.Normal]) {
+  if (text == 'normal') return TagType.Normal;
+  if (text == 'important') return TagType.Important;
+  return defaultValue;
+}
+String TagType_toString(TagType value, [String defaultText = 'normal']) {
+  if (value == TagType.Normal) return 'normal';
+  if (value == TagType.Important) return 'important';
+  return defaultText;
 }
