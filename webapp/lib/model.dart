a1ebb450d03f2b4996ee2ee174564ec745f1993d
Dan Rubel <dan@lark.systems>
2019-10-18 14:33:27 -0400

    move SuggestedReply and Tag into model.g.dart (#135)

--- webapp/lib/model.dart
+++ webapp/lib/model.dart
@@ -8,7 +8,7 @@ class DeidentifiedPhoneNumber {
 class Conversation {
   DeidentifiedPhoneNumber deidentifiedPhoneNumber;
   Map<String, String> demographicsInfo;
-  List<Tag> tags;
+  List<g.Tag> tags;
   List<Message> messages;
   String notes;
 }
@@ -18,21 +18,7 @@ class Message {
   DateTime datetime;
   String text;
   String translation;
-  List<Tag> tags;
-}
-
-class SuggestedReply {
-  String suggestedReplyId;
-  String text;
-  String translation;
-  String shortcut;
-}
-
-class Tag {
-  String tagId;
-  String text;
-  g.TagType type;
-  String shortcut;
+  List<g.Tag> tags;
 }
 
 class User {
