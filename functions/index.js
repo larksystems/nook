dd4b59bde8712188fdf0ef237dab1eff5e5321b1
danrubel <dan.rubel.usa@gmail.com>
2019-12-21 15:51:43 -0500

    add explaination and links to firebase docs

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,9 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+// See https://firebase.google.com/docs/functions/get-started
 const functions = require('firebase-functions');
 
-// The Firebase Admin SDK to authenticate publish requests.
+// The Firebase Admin SDK to authenticate incomming publish requests
+// before calling google-cloud/pubsub.
 const admin = require('firebase-admin');
 admin.initializeApp();
 
@@ -51,16 +53,23 @@ exports.Publish = functions.https.onRequest((req, res) => {
         payload: data.payload,
     };
 
+    // Validate the user making the request then forward the pub/sub message to google cloud.
+
     fbUserIdToken = data.fbUserIdToken;
     console.log(`Validating: ${fbUserIdToken}`);
 
-    // Validate the user making the request
+    // Step 1) Decode and validate the encrypted/signed JWT (JSON web token)
+    // If token is valid, returns a promise which returns a decoded token passed into next "then" block.
+    // If token is NOT valid, then an exception is thrown.
+    // See https://firebase.google.com/docs/auth/admin/verify-id-tokens#verify_id_tokens_using_the_firebase_admin_sdk
     admin.auth()
       .verifyIdToken(data.fbUserIdToken)
       .then((decodedToken) => {
-        // Lookup the user
         userId = decodedToken.uid;
         console.log(`Looking up: ${userId}`);
+        // Step 2) Look up the user to obtain the email address for logging purposes.
+        // Returns promise which returns a UserRecord passed as "user" into next "then" block.
+        // https://firebase.google.com/docs/reference/admin/node/admin.auth.UserRecord.html
         return admin.auth().getUser(userId);
       }).then((user) => {
         // Publish the message
@@ -73,7 +82,7 @@ exports.Publish = functions.https.onRequest((req, res) => {
       }).catch(err => {
         // Log the error and respond indicating failure
         console.error(err);
-        res.status(500).send(err);
+        return res.status(500).send(err);
       });
     });
   });
