ab12c2fcfa993cd9f4ad7c896f44f0e31d7603f0
danrubel <dan.rubel.usa@gmail.com>
2020-02-28 15:48:59 -0500

    update save note functionality

--- webapp/lib/controller.dart
+++ webapp/lib/controller.dart
@@ -436,13 +436,15 @@ void command(UIAction action, Data data) {
         );
       }
       break;
-    case UIAction.updateNote:
-      var conversation = activeConversation;
-      SaveTextAction.textChange(
-        "${conversation.docId}.notes",
-        (data as NoteData).noteText,
-        (newText) => platform.updateNotes(conversation, newText),
-      );
+    case UIAction.updateNote: {
+        // inside block to ensure conversation is a local/captured var
+        var conversation = activeConversation;
+        SaveTextAction.textChange(
+          "${conversation.docId}.notes",
+          (data as NoteData).noteText,
+          (newText) => platform.updateNotes(conversation, newText),
+        );
+      }
       break;
     case UIAction.userSignedOut:
       signedInUser = null;
