ed6e17a0e1a2abeb8b21e573f1eaabed0ba3d71c
Mariana Marasoiu <mariana.marasoiu@gmail.com>
2020-03-20 11:35:50 +0000

    Add needs reply analytics

--- tool/compute_tag_analytics.py
+++ tool/compute_tag_analytics.py
@@ -5,7 +5,7 @@ import sys
 import time
 
 from core_data_modules.logging import Logger
-from datetime import datetime
+from datetime import datetime, timezone
 
 import demogs_helper as demogs
 
@@ -14,6 +14,8 @@ log = Logger(__name__)
 CONVERSATIONS_COLLECTION_KEY = 'nook_conversations'
 CONVERSATION_TAGS_COLLECTION_KEY = 'conversationTags'
 DAILY_TAG_METRICS_COLLECTION_KEY = 'daily_tag_metrics'
+
+NEEDS_REPLY_TAG = "Needs Reply"
 
 coda_tags = {}
 
@@ -114,6 +116,54 @@ def compute_daily_tag_distribution(nook_conversations, ignore_stop=False):
     return daily_metrics
 
 
+def compute_needs_reply(nook_conversations):
+    log.info (f"compute_needs_reply: processing {len(nook_conversations)} conversations...")
+
+    time_start = time.perf_counter_ns()
+
+    needs_reply_count = 0
+    needs_reply_dates = []
+    needs_reply_tag_id = tag_to_tag_id(NEEDS_REPLY_TAG)
+
+    def get_last_incoming_message(messages):
+        for message in messages[::-1]:
+            if message["direction"] == "MessageDirection.out":
+                continue
+            return message["datetime"]
+
+    for conversation in nook_conversations:
+        conversation_tags = conversation["tags"]
+
+        if needs_reply_tag_id not in conversation_tags:
+            continue
+
+        # Increase needs reply count
+        needs_reply_count += 1
+
+        # Get the date of the last incoming message
+        needs_reply_dates.append(get_last_incoming_message(conversation["messages"]))
+
+
+    earliest_date = datetime.now(timezone.utc)
+    for date in needs_reply_dates:
+        iso_date = datetime.fromisoformat(date)
+        if iso_date < earliest_date:
+            earliest_date = iso_date
+
+    needs_reply_metrics = {
+        "datetime": datetime.now().isoformat(),
+        "needs_reply_count": needs_reply_count,
+        "needs_reply_dates": needs_reply_dates,
+        "earliest_needs_reply_date": earliest_date.isoformat(),
+    }
+
+    time_end = time.perf_counter_ns()
+
+    ms_elapsed = (time_end - time_start) / (1000 * 1000)
+    log.info (f"compute_needs_reply: processed {len(nook_conversations)} conversations in {ms_elapsed} ms")
+
+    return needs_reply_metrics
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("nook_export",
@@ -177,3 +227,15 @@ if __name__ == '__main__':
     with open(daily_metrics_file, mode="w", encoding='utf-8') as output_file:
         json.dump(daily_metrics_json, output_file, indent=2)
         log.info(f"compute_daily_tag_distribution saved to {daily_metrics_file}")
+
+    needs_reply_metrics = compute_needs_reply(nook_conversations)
+    # prepare for writing to a json file that can be uploaded to firebase
+    needs_reply_metrics["__id"] = "latest_needs_reply_metrics"
+    needs_reply_metrics["__reference_path"] = f"{DAILY_TAG_METRICS_COLLECTION_KEY}/{day}"
+    needs_reply_metrics["__subcollections"] = []
+    needs_reply_metrics_json = {'latest_needs_reply_metrics' : needs_reply_metrics}
+
+    needs_reply_metrics_file = f"{OUTPUT_FOLDER}/nook-analysis-needs_reply_metrics_{now}.json"
+    with open(needs_reply_metrics_file, mode="w", encoding='utf-8') as output_file:
+        json.dump(needs_reply_metrics_json, output_file, indent=2)
+        log.info(f"compute_needs_reply saved to {needs_reply_metrics_file}")
