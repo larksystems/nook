be7c549f7a22683132c5024c0bcc011780e04051
Kenneth Thuku <thuku.ken@live.com>
2020-04-27 16:38:56 +0300

    Integrated firebase initialization from firebase util

--- tool/clear_firebase.py
+++ tool/clear_firebase.py
@@ -1,30 +1,16 @@
 # Temporary tool that reads from Firebase and writes a JSON file
 # Relies on an undocumented .collections() API call and a hard coded list of top level collections
 
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-from katikati_pylib.logging import logging
-from firebase_root_keys import root_keys
 import time
-import tool_utils
-
 import json
 import sys
 
-log = None
+import firebase_util
+from firebase_util import firebase_client
+from firebase_util import log
+from firebase_root_keys import root_keys
+import tool_utils
 
-firebase_client = None
-
-def init(CRYPTO_TOKEN_PATH):
-    global firebase_client
-    global log
-    log = logging.Logger(__file__, CRYPTO_TOKEN_PATH)
-    log.info("Setting up Firebase client")    
-    firebase_cred = credentials.Certificate(CRYPTO_TOKEN_PATH)
-    firebase_admin.initialize_app(firebase_cred)
-    firebase_client = firestore.client()
-    log.info("Done")
 
 def delete_collection_twophase_strategy(col):
     log.info (f"processing: {col._path}")
@@ -81,7 +67,7 @@ if __name__ == '__main__':
         exit(1)
 
     CRYPTO_TOKEN_PATH = sys.argv[1]
-    init(CRYPTO_TOKEN_PATH)
+    firebase_util.init(CRYPTO_TOKEN_PATH)
 
     data = {}
 
