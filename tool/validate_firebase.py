a1c0ed21b4bafdb5bb8c58f49611e940c9e1764a
Kenneth Thuku <thuku.ken@live.com>
2020-04-28 12:00:15 +0300

    Integrated firebase initialization from firebase util

--- tool/validate_firebase.py
+++ tool/validate_firebase.py
@@ -1,30 +1,15 @@
 # Validation tool for firebase collections
 # Relies on an undocumented .collections() API call and a hard coded list of top level collections
 
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-from core_data_modules.logging import Logger
 import time
-
 import json
 import sys
 import os.path
 
 import validate_nook_model as model
 import validate_nook_model_custom as custom
-
-log = Logger(__name__)
-
-firebase_client = None
-
-def init(crypto_token_path):
-    global firebase_client
-    log.info("Setting up Firebase client")    
-    firebase_cred = credentials.Certificate(crypto_token_path)
-    firebase_admin.initialize_app(firebase_cred)
-    firebase_client = firestore.client()
-    log.info("Done")
+import firebase_util
+from firebase_util import firebase_client, log, LoggerType
 
 def validate_documents(collection_root, validationMethod):
     log.info (f"validate_documents {collection_root}")
@@ -64,7 +49,7 @@ if not os.path.isfile(crypto_token_path):
     usage()
     exit(1)
 
-init(crypto_token_path)
+firebase_util.init(crypto_token_path, LoggerType.CORE_DATA_MODULES)
 validate_documents("systemMessages",     model.validate_SystemMessage_doc)
 validate_documents("suggestedReplies",   model.validate_SuggestedReply_doc)
 validate_documents("conversationTags",   custom.validate_ConversationTag)
