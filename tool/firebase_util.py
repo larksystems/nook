04c6a4e223d73e199df69b3301391b0929efe92a
Kenneth Thuku <thuku.ken@live.com>
2020-04-29 16:59:33 +0300

    firebase_client init returns firebase_client instance

--- tool/firebase_util.py
+++ tool/firebase_util.py
@@ -4,12 +4,10 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-firebase_client = None
-
 def init(CRYPTO_TOKEN_PATH, log):
-    global firebase_client
     log.info("Setting up Firebase client")
     firebase_cred = credentials.Certificate(CRYPTO_TOKEN_PATH)
     firebase_admin.initialize_app(firebase_cred)
     firebase_client = firestore.client()
     log.info("Done")
+    return firebase_client
