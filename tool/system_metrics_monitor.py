9639312631a9472629fb8d01237c567237711d1f
Kenneth Thuku <thuku.ken@live.com>
2020-05-04 12:09:52 +0300

    Remove passing of log parameter into init_firebase_client function

--- tool/system_metrics_monitor.py
+++ tool/system_metrics_monitor.py
@@ -71,7 +71,7 @@ def run_system_metric_monitor(interval=DEFAULT_INTERVAL):
     parser.add_argument("crypto_token_file", type=str, help="path to Firebase crypto token file")
     args = parser.parse_args()
 
-    firebase_client = init_firebase_client(args.crypto_token_file, log)
+    firebase_client = init_firebase_client(args.crypto_token_file)
     runner = threading.Thread(target=get_and_publish_system_metrics, args=(interval,))
     runner.start()
 
