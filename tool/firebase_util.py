dadc486550ff8b6b370b11773390079f37e37046
Kenneth Thuku <thuku.ken@live.com>
2020-05-05 12:29:05 +0300

    Move push_collection_to_firestore and push_document_to_firestore from json_to_firebase.py to firebase_util.py

--- tool/firebase_util.py
+++ tool/firebase_util.py
@@ -1,14 +1,54 @@
+import time
 
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 from katikati_pylib.logging import logging
 
+log = None
+
 def init_firebase_client(CRYPTO_TOKEN_PATH):
-    log = logging.Logger(__file__, CRYPTO_TOKEN_PATH)
+    global log
+    if log is None:
+        log = logging.Logger(__file__, CRYPTO_TOKEN_PATH)
     log.info("Setting up Firebase client")
     firebase_cred = credentials.Certificate(CRYPTO_TOKEN_PATH)
     firebase_admin.initialize_app(firebase_cred)
     firebase_client = firestore.client()
     log.info("Done")
     return firebase_client
+
+def push_collection_to_firestore(collection_root, documents, firebase_client, CRYPTO_TOKEN_PATH):
+    global log
+    if log is None:
+        log = logging.Logger(__file__, CRYPTO_TOKEN_PATH)
+    log.info (f"push_collection_to_firestore {collection_root}")
+    col = firebase_client.collection(collection_root)
+    time_start = time.perf_counter_ns()
+    for document_dict in documents:
+        push_document_to_firestore(collection_root, document_dict, firebase_client, CRYPTO_TOKEN_PATH)
+    time_end = time.perf_counter_ns()
+    ms_elapsed = (time_end - time_start) / (1000 * 1000)
+    log.info (f"push_collection_to_firestore {collection_root} in {ms_elapsed} ms")
+
+def push_document_to_firestore(collection_root, data,firebase_client, CRYPTO_TOKEN_PATH):
+    ref_path = data["__reference_path"]
+    doc_id = data["__id"]
+    sub_collections = data["__subcollections"]
+
+    log.info (f"push_document_to_firestore {ref_path}")
+
+    del data["__reference_path"]
+    del data["__id"]
+    del data["__subcollections"]
+
+    field_data = {}
+    for field in data:
+        if field in sub_collections:
+            # it's not actually a field, it's a collection, process these later
+            continue
+        field_data[field] = data[field]
+    firebase_client.document(ref_path).set(field_data)
+
+    for sub_collection in sub_collections:
+        push_collection_to_firestore(f"{ref_path}/{sub_collection}", data[sub_collection], firebase_client, CRYPTO_TOKEN_PATH)
--- tool/json_to_firebase.py
+++ tool/json_to_firebase.py
@@ -6,44 +6,12 @@ import argparse
 import json
 import sys
 
-from firebase_util import init_firebase_client
+from firebase_util import init_firebase_client, push_collection_to_firestore
 from katikati_pylib.logging import logging
 import tool_utils
 
 log = None
 firebase_client = None
-
-def push_collection_to_firestore(collection_root, documents):
-    log.info (f"push_collection_to_firestore {collection_root}")
-    col = firebase_client.collection(collection_root)
-    time_start = time.perf_counter_ns()
-    for document_dict in documents:
-        push_document_to_firestore(collection_root, document_dict)
-    time_end = time.perf_counter_ns()
-    ms_elapsed = (time_end - time_start) / (1000 * 1000)
-    log.info (f"push_collection_to_firestore {collection_root} in {ms_elapsed} ms")
-
-def push_document_to_firestore(collection_root, data):
-    ref_path = data["__reference_path"]
-    doc_id = data["__id"]
-    sub_collections = data["__subcollections"]
-
-    log.info (f"push_document_to_firestore {ref_path}")
-
-    del data["__reference_path"]
-    del data["__id"]
-    del data["__subcollections"]
-
-    field_data = {}
-    for field in data:
-        if field in sub_collections:
-            # it's not actually a field, it's a collection, process these later
-            continue
-        field_data[field] = data[field]
-    firebase_client.document(ref_path).set(field_data)
-
-    for sub_collection in sub_collections:
-        push_collection_to_firestore(f"{ref_path}/{sub_collection}", data[sub_collection])
 
 
 if __name__ == '__main__':
@@ -81,7 +49,7 @@ if __name__ == '__main__':
 
     for collection_root in collection_keys:
         documents = data_dict[collection_root]
-        push_collection_to_firestore(collection_root, documents)
+        push_collection_to_firestore(collection_root, documents, firebase_client, CRYPTO_TOKEN_PATH)
 
     log.info(f"Import done")
     log.notify(f"json_to_firebase completed: JobID {short_id}")
