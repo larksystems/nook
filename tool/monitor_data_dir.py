6a3f86d8917da358f47bc0a71813290ea146aaaa
Kenneth Thuku <thuku.ken@live.com>
2020-05-06 17:34:30 +0300

    Script to monitor size of data directory

---
+++ tool/monitor_data_dir.py
@@ -0,0 +1,33 @@
+import argparse
+import os
+import time
+import threading
+
+from firebase_util import init_firebase_client
+from data_dir_stat import get_size_and_upload
+
+CRYPTO_TOKEN_PATH = None
+DEFAULT_INTERVAL = 600
+project = None
+dirpath = None
+firebase_client = None
+
+def run():
+    while True:
+        get_size_and_upload(CRYPTO_TOKEN_PATH, dirpath, project, firebase_client)
+        time.sleep(3) 
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Monitor data dir size")
+    parser.add_argument("crypto_token_file", type=str, help="path to Firebase crypto token file")
+    parser.add_argument("project", type=str, help="name of the project")
+    parser.add_argument("dirname", type=str, help="name of the directory to monitor")
+    args = parser.parse_args()
+    
+    CRYPTO_TOKEN_PATH = args.crypto_token_file
+    dirpath = os.path.abspath(args.dirname)
+    project = args.project
+    firebase_client = init_firebase_client(CRYPTO_TOKEN_PATH)
+    runner = threading.Thread(target=run)
+    runner.start()
