9d788cf5d5a8aa04d5ca0534af4cecd41d1b9047
Kenneth Thuku <thuku.ken@live.com>
2020-05-13 16:13:56 +0300

    Refactor code that pushes document to firebase and add doc comments

--- tool/firebase_util.py
+++ tool/firebase_util.py
@@ -26,36 +26,56 @@ def push_collection_to_firestore(collection_root, documents):
     col = firebase_client.collection(collection_root)
     time_start = time.perf_counter_ns()
     for document_dict in documents:
-        push_document_to_firestore(collection_root, document_dict)
+        if "__id" in document_dict:
+            _push_document_to_firestore(collection_root, document_dict)
+        else:
+            _push_document_fields_to_firestore(collection_root, document_dict)
     time_end = time.perf_counter_ns()
     ms_elapsed = (time_end - time_start) / (1000 * 1000)
     log.info (f"push_collection_to_firestore {collection_root} in {ms_elapsed} ms")
 
-def push_document_to_firestore(collection_root, data):
-    if "__id" in data:
-        ref_path = data["__reference_path"]
-        doc_id = data["__id"]
-        sub_collections = data["__subcollections"]
 
-        log.info (f"push_document_to_firestore {ref_path}")
+"""
+Example of document structure
+{
+    "field1": "",
+    "field2": "",
+    "__id": "doc-1",
+    "__reference_path": "collection-1/doc-1",
+    "__subcollections": []
+},
+"""
+def _push_document_to_firestore(collection_root, data):
+    ref_path = data["__reference_path"]
+    doc_id = data["__id"]
+    sub_collections = data["__subcollections"]
 
-        del data["__reference_path"]
-        del data["__id"]
-        del data["__subcollections"]
+    log.info (f"push_document_to_firestore {ref_path}")
 
-        field_data = {}
-        for field in data:
-            if field in sub_collections:
-                # it's not actually a field, it's a collection, process these later
-                continue
-            field_data[field] = data[field]
-        firebase_client.document(ref_path).set(field_data)
+    del data["__reference_path"]
+    del data["__id"]
+    del data["__subcollections"]
 
-        for sub_collection in sub_collections:
-            push_collection_to_firestore(f"{ref_path}/{sub_collection}", data[sub_collection])
-    else:
-        _push_document_fields_to_firestore(collection_root, data)
+    field_data = {}
+    for field in data:
+        if field in sub_collections:
+            # it's not actually a field, it's a collection, process these later
+            continue
+        field_data[field] = data[field]
+    firebase_client.document(ref_path).set(field_data)
 
+    for sub_collection in sub_collections:
+        push_collection_to_firestore(f"{ref_path}/{sub_collection}", data[sub_collection])
+
+"""
+Example of document structure
+{
+    "doc-1": {
+      "field1": "",
+      "field2": "",
+    }
+  }
+"""
 def _push_document_fields_to_firestore(collection_root, data):
     doc_id = list(data.keys())[0]
     doc_fields = data[doc_id]
