5928a2cdb7b878ad37affe82b4c69ac15477417b
Kenneth Thuku <thuku.ken@live.com>
2020-05-13 14:08:52 +0300

    Store global reference to firebase_client

--- tool/firebase_util.py
+++ tool/firebase_util.py
@@ -15,7 +15,9 @@ def init_firebase_client(CRYPTO_TOKEN_PATH):
     log.info("Setting up Firebase client")
     firebase_cred = credentials.Certificate(CRYPTO_TOKEN_PATH)
     firebase_admin.initialize_app(firebase_cred)
-    firebase_client = firestore.client()
+    global firebase_client
+    if firebase_client is None:
+        firebase_client = firestore.client()
     log.info("Done")
     return firebase_client
 
