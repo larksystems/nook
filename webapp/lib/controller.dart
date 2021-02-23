7ef5c5dd88397e114d4e961e388a836e63a6db92
EB <elayabharath@gmail.com>
2021-02-23 17:10:17 +0530

    Remove should disable replies

--- webapp/lib/controller.dart
+++ webapp/lib/controller.dart
@@ -491,48 +491,44 @@ void initUI() {
       }
     }, showAndLogError);
 
-  if (view.urlView.shouldDisableReplies) {
-    view.replyPanelView.disableReplies();
-  } else {
-    platform.listenForSuggestedReplies(
-      (added, modified, removed) {
-        var updatedIds = new Set()
-          ..addAll(added.map((r) => r.suggestedReplyId))
-          ..addAll(modified.map((r) => r.suggestedReplyId))
-          ..addAll(removed.map((r) => r.suggestedReplyId));
-        suggestedReplies.removeWhere((suggestedReply) => updatedIds.contains(suggestedReply.suggestedReplyId));
-        suggestedReplies
-          ..addAll(added)
-          ..addAll(modified);
+  platform.listenForSuggestedReplies(
+    (added, modified, removed) {
+      var updatedIds = new Set()
+        ..addAll(added.map((r) => r.suggestedReplyId))
+        ..addAll(modified.map((r) => r.suggestedReplyId))
+        ..addAll(removed.map((r) => r.suggestedReplyId));
+      suggestedReplies.removeWhere((suggestedReply) => updatedIds.contains(suggestedReply.suggestedReplyId));
+      suggestedReplies
+        ..addAll(added)
+        ..addAll(modified);
 
-        // Update the replies by category map
-        suggestedRepliesByCategory = _groupRepliesIntoCategories(suggestedReplies);
-        // Empty sublist if there are no replies to show
-        if (suggestedRepliesByCategory.isEmpty) {
-          suggestedRepliesByCategory[''] = [];
-        }
-        // Sort by sequence number
-        for (var replies in suggestedRepliesByCategory.values) {
-          replies.sort((r1, r2) {
-            var seqNo1 = r1.seqNumber == null ? double.nan : r1.seqNumber;
-            var seqNo2 = r2.seqNumber == null ? double.nan : r2.seqNumber;
-            return seqNo1.compareTo(seqNo2);
-          });
-        }
-        List<String> categories = suggestedRepliesByCategory.keys.toList();
-        categories.sort((c1, c2) => c1.compareTo(c2));
-        // Replace list of categories in the UI selector
-        view.replyPanelView.categories = categories;
-        // If the categories have changed under us and the selected one no longer exists,
-        // default to the first category, whichever it is
-        if (!categories.contains(selectedSuggestedRepliesCategory)) {
-          selectedSuggestedRepliesCategory = categories.first;
-        }
-        // Select the selected category in the UI and add the suggested replies for it
-        view.replyPanelView.selectedCategory = selectedSuggestedRepliesCategory;
-        _populateReplyPanelView(suggestedRepliesByCategory[selectedSuggestedRepliesCategory]);
-      }, showAndLogError);
-  }
+      // Update the replies by category map
+      suggestedRepliesByCategory = _groupRepliesIntoCategories(suggestedReplies);
+      // Empty sublist if there are no replies to show
+      if (suggestedRepliesByCategory.isEmpty) {
+        suggestedRepliesByCategory[''] = [];
+      }
+      // Sort by sequence number
+      for (var replies in suggestedRepliesByCategory.values) {
+        replies.sort((r1, r2) {
+          var seqNo1 = r1.seqNumber == null ? double.nan : r1.seqNumber;
+          var seqNo2 = r2.seqNumber == null ? double.nan : r2.seqNumber;
+          return seqNo1.compareTo(seqNo2);
+        });
+      }
+      List<String> categories = suggestedRepliesByCategory.keys.toList();
+      categories.sort((c1, c2) => c1.compareTo(c2));
+      // Replace list of categories in the UI selector
+      view.replyPanelView.categories = categories;
+      // If the categories have changed under us and the selected one no longer exists,
+      // default to the first category, whichever it is
+      if (!categories.contains(selectedSuggestedRepliesCategory)) {
+        selectedSuggestedRepliesCategory = categories.first;
+      }
+      // Select the selected category in the UI and add the suggested replies for it
+      view.replyPanelView.selectedCategory = selectedSuggestedRepliesCategory;
+      _populateReplyPanelView(suggestedRepliesByCategory[selectedSuggestedRepliesCategory]);
+    }, showAndLogError);
 
   platform.listenForConversationListShards(
     (added, modified, removed) {
--- webapp/lib/view.dart
+++ webapp/lib/view.dart
@@ -1545,6 +1545,7 @@ class ReplyPanelView {
     }
   }
 
+  // todo: make this call based on the user config
   void disableReplies() {
     _replies.remove();
     _panelTitle.children.clear();
@@ -1994,7 +1995,6 @@ class MarkUnreadActionView {
 
 class UrlView {
 
-  static const String queryDisableRepliesKey = 'disableReplies';
   static const String queryConversationListKey = 'conversation-list';
   static const String queryConversationIdKey = 'conversation-id';
   static const String queryConversationIdFilterKey = 'conversation-id-filter';
@@ -2112,14 +2112,6 @@ class UrlView {
     }
     uri = uri.replace(queryParameters: queryParameters);
     window.history.pushState('', '', uri.toString());
-  }
-
-  bool get shouldDisableReplies {
-    var uri = Uri.parse(window.location.href);
-    if (uri.queryParameters.containsKey(queryDisableRepliesKey)) {
-      return uri.queryParameters[queryDisableRepliesKey].toLowerCase() == 'true';
-    }
-    return false;
   }
 
   String getPageUrlFilterConversationId() {
