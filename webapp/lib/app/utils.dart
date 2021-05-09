492ae512e1d123461c0b0f89a31962eff218437b
EB <elayabharath@gmail.com>
2021-05-09 14:09:04 +0530

    Fixed dashboard to have separate link

--- webapp/lib/app/utils.dart
+++ webapp/lib/app/utils.dart
@@ -1,7 +1,8 @@
 import 'package:katikati_ui_lib/components/nav/button_links.dart';
 
 var navLinks = [
-  Link("Dashboard", "/converse/index.html"),
-  Link("Standard messages", "/configure/messages.html"),
+  Link("Dashboard", "/"),
+  Link("Conversations", "/converse/index.html"),
+  Link("Messages", "/configure/messages.html"),
   Link("Tags", "/configure/tags.html")
 ];
